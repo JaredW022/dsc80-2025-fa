8068d15feec6710ceb89d8f39f8371528bf05f40
JaredW022 <jjw022@ucsd.edu>
2025-11-13 13:15:08 -0800

    Co-authored-by: Jacob Takesuye <jaketakes@users.noreply.github.com>

--- projects/proj03/project.py
+++ projects/proj03/project.py
@@ -15,7 +15,14 @@ import time
 
 
 def get_book(url):
-    ...
+    response = requests.get(url)
+    time.sleep(0.5)
+    text = response.text
+    start = text.find(re.search(r"\*\*\* START OF THE PROJECT GUTENBERG EBOOK (.*?) \*\*\*", text).group(0)) + len(re.search(r"\*\*\* START OF THE PROJECT GUTENBERG EBOOK (.*?) \*\*\*", text).group(0))
+    stop = text.find(re.search(r"\*\*\* END OF THE PROJECT GUTENBERG EBOOK (.*?) \*\*\*", text).group(0), start)
+    stripped = text[start:stop]
+    stripped = stripped.replace("\r\n", "\n")
+    return stripped
 
 
 # ---------------------------------------------------------------------
@@ -31,22 +38,35 @@ def tokenize(book_string):
 # QUESTION 3
 # ---------------------------------------------------------------------
 
-
 class UniformLM(object):
 
-
     def __init__(self, tokens):
+            self.mdl = self.train(tokens)
 
-        self.mdl = self.train(tokens)
-        
     def train(self, tokens):
-        ...
+        self.dct = {}
+        total = 0
+        for token in tokens:
+            if token not in self.dct.keys():
+                self.dct.update({token: 1})
+                total += 1
+        for key in self.dct:
+            current_val = self.dct.get(key)
+            current_val /= total
+            self.dct.update({key: current_val})
+        return pd.Series(self.dct)
     
     def probability(self, words):
-        ...
-        
+        probs = []
+        for word in words:
+            if word not in self.mdl:
+                return 0.0
+            probs.append(self.mdl[word])
+        return np.prod(probs)
+    
     def sample(self, M):
-        ...
+        words = np.random.choice(self.mdl.index, M, p=self.mdl.values)
+        return " ".join(words)
 
 
 # ---------------------------------------------------------------------
@@ -55,18 +75,39 @@ class UniformLM(object):
 
 
 class UnigramLM(object):
-    
+
     def __init__(self, tokens):
-        self.mdl = self.train(tokens)
-    
+
+            self.mdl = self.train(tokens)
+
     def train(self, tokens):
-        ...
+        self.dct = {}
+        total = 0
+        for token in tokens:
+            if token in self.dct.keys():
+                current_val = self.dct.get(token)
+                self.dct.update({token: current_val + 1})
+                total += 1
+            else:
+                self.dct.update({token: 1})
+                total += 1
+        for key in self.dct:
+            current_val = self.dct.get(key)
+            current_val /= total
+            self.dct.update({key: current_val})
+        return pd.Series(self.dct)
     
     def probability(self, words):
-        ...
-        
+        probs = []
+        for word in words:
+            if word not in self.mdl:
+                return 0.0
+            probs.append(self.mdl[word])
+        return np.prod(probs)
+    
     def sample(self, M):
-        ...
+        words = np.random.choice(self.mdl.index, M, p=self.mdl.values)
+        return " ".join(words)
 
 
 # ---------------------------------------------------------------------
@@ -95,7 +136,14 @@ class NGramLM(object):
             self.prev_mdl = NGramLM(N-1, tokens)
 
     def create_ngrams(self, tokens):
-        ...
+        start_point = 0
+        end_point = self.N
+        ngrams = []
+        while end_point <= len(tokens):
+            ngrams.append(tuple(tokens[start_point:end_point]))
+            start_point += 1
+            end_point += 1
+        return ngrams
         
     def train(self, ngrams):
         ...
